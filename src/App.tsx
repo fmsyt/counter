0c028762d0cc0e52fcaacb58ec0eab3d94607ae9
motsuni <motsuni@yotta-rc.dev>
2024-07-07 19:13:41 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Sequence from "./Sequence";
+
+function App() {
+
+  return (
+    <div className="p-4 h-svh">
+      <div className="grid place-content-center h-full">
+        <Sequence />
+      </div>
+    </div>
+  );
+}
+
+export default App;
