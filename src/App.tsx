0f2705249d9f1a5540208aacffee17ba16a6f9da
motsuni <motsuni@yotta-rc.dev>
2024-07-07 23:53:48 +0900

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import Objective from "./Objective";
 import Sequence from "./Sequence";
 
 function App() {
 
   return (
     <div className="p-4 h-svh">
-      <div className="grid place-content-center h-full">
+      <div className="grid gap-4 place-content-center h-full">
         <Sequence />
+
+        <Objective />
       </div>
     </div>
   );
---
+++ src/Objective.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useState } from "react";
+
+export default function Objective() {
+
+  const [count, setCount] = useState(0)
+  const [goal, setGoal] = useState(10)
+
+  const increment = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+
+  const decrement = useCallback(() => {
+    setCount((prev) => {
+      if (prev > 0) {
+        return prev - 1;
+      }
+      return prev;
+    });
+  }, []);
+
+  const reset = useCallback(() => {
+    setCount(0);
+  }, []);
+
+  const getProgressColor = useCallback((count: number, goal: number) => {
+    if (count === goal) {
+      return "progress-success";
+    }
+
+    if (count > goal) {
+      return "progress-warning";
+    }
+
+    return "progress-primary";
+  }, [])
+  return (
+    <div>
+      <div
+        className="flex items-center justify-center gap-4"
+      >
+        <button
+          className="btn btn-outline btn-circle"
+          onClick={decrement}
+        >
+          <span className="material-symbols-outlined">
+            remove
+          </span>
+        </button>
+
+        <div className="grid grid-cols-[1fr,_1em,_1fr]">
+          <span className="text-right">{count}</span>
+          <span className="text-center">/</span>
+          <span className="text-right">{goal}</span>
+        </div>
+
+        <button
+          className="btn btn-outline btn-circle"
+          onClick={increment}
+        >
+          <span className="material-symbols-outlined">
+            add
+          </span>
+        </button>
+      </div>
+
+      <progress
+        className={`progress ${getProgressColor(count, goal)} w-56`}
+        value={count}
+        max={goal}
+      />
+    </div>
+  )
+}
